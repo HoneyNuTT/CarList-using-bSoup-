62e6e1f8727c1d53891577ba07af5ade5f945bd2
Aminhanniel Moncayo <AminhannielMoncayo99@gmail.com>
2022-12-17 19:10:44 -0500

    A lot of learning, getting better at python which is pretty cool

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import string
 from bs4 import BeautifulSoup
 import webbrowser
@@ -5,64 +6,115 @@ import requests
 from CarMake import CarMake
 
 # GRAB LINKS FROM URL + MAKE + MODEL 
-def getLinks():
-    if isMake == True:
-        print("Looking for data from: ", url+linksOfCarMakes[0])
-        result = requests.get(url+linksOfCarMakes[0])
-    else:
-        print("Looking for data from: ", url)
+def getLinks(url, forMake, forModel, listOfCarMakeLinks = None, listOfCarModelLinks = None):
+    #Links for Make
+    if forMake == True and forModel == False:
+        global completedList
+        print("Looking for data from: ", url, '\n')
         result = requests.get(url)
+        completedList = bSParser(result, forModel)
+    #Links for Model
+    elif forMake == False and forModel == True:
+        for index, make in enumerate(listOfCarMakeLinks):
+            if index < 56:
+                print("Looking for data from: ", url + make, '\n')
+                result = requests.get(url + make)
+                result = bSParser(result, forModel)
+                completedList.extend(result)
+            
+    #Default 
+    else:
+        print("Looking for data from: ", url, '\n')
+        result = requests.get(url)
+    return completedList
 
+
+def bSParser(result, forModel = None):
     soup = BeautifulSoup(result.text, "html.parser")
     List = [a['href'] for a in soup.find_all('a', href=True)]
     List = [x for x in List if "car-specs" in x]
     List = [x for x in List if "www" not in x]
-    List.pop()
-
-    if isModel == True:
+    if forModel == True and bool(List) == True:
         List.pop(0)
-
+    elif forModel == False and bool(List) == True:
+        List.pop()
     return List
 
 # EXTRACT CAR MODEL OR CAR MAKE FROM LINKS 
-def extractFromLinks(listOfLinks, isModel: list[string]) -> list[string]:
-    if isModel == True:
-         return [x.split('/')[2] for x in listOfLinks]
-    elif isModel == False:
-        return [x.split('/')[3] for x in listOfLinks]
+def extractFromLinks(listOfLinks, forMake, forModel = None):
+    extractedList = []
+    if forMake == True and forModel == False:
+        forModel = True
+        return [make.split('/')[2] for make in listOfLinks]
 
-
+    elif forMake == False and forModel == True:
+        return [model.split('/')[3] for model in listOfLinks]
 
 #NEED A FUNCION THAT LOOP THROUGH THE ENTIRE LIST AND CALLS THE GET LINKS FUNCTION FOR EVERY CAR IN THE MAKE
-def buildListFromLinks():
-    if isMake == False and isModel == False:
-        global linksOfCarMakes
-        linksOfCarMakes = getLinks()
-        print(*linksOfCarMakes, sep='\n')
-        isMake = True
+def buildList(url, forMake, forModel, sizeOfCarMakes = None, listofCarMakes = None, listOfCarModels = None):
+    #Get Links for CarMakes
+    if forMake == True and forModel == False:
+        linksOfCarMakes = getLinks(url, forMake, forModel)
+        sizeOfCarMakes = len(linksOfCarMakes)
+        forMake = False
+        forModel = True
+        return linksOfCarMakes, sizeOfCarMakes, forMake, forModel
+    #Get Links for CarModel
+    elif forMake == False and forModel == True:
+        #Grab Links for Car Models
+        linksForCarModels = getLinks(url, forMake, forModel, listofCarMakes)
+        return linksForCarModels
 
 
 
 
-
-global url
-global isModel
-global isMake
-isModel = False
-isMake = False 
+forModel = False
+forMake = True 
 url = "https://www.cars-directory.net/"
+sizeOfCarMakes = 0
 
 
-buildListFromLinks()
+#Build Inital list of carMakes + grab the Length of list
+#linksOfCarMakes, sizeOfCarMakes, forMake, forModel = buildList(url, forMake, forModel)
+
+
+#linksofCarModels = buildList(url, forMake, forModel, sizeOfCarMakes, linksOfCarMakes)
+#print(*linksofCarModels, sep='\n')
+
+myFile = open("FileToExport.txt", "r")
+data = myFile.read()
+data = data.split('\n')
+print(type(data))
+print(len(data))
+
+newData = data[:56]
+newData = extractFromLinks(newData, forMake,forModel)
+print(newData)
+
+#newerData = data[55:]
+#forModel = True
+#forMake = False
+#print(newerData[1])
+#print(len(newerData))
+#newerData = extractFromLinks(newerData, forMake,forModel)
+#print(newerData)
 
 
 
 
-#with open("FileToExport.txt", 'w') as f:
- #   for x in listOfCarMakeNames:
-  #      f.write(x)
-   #     f.write('\n')
-   # print("Data Exported")
+#print(data)
+
+#with open("filetoexport.txt", 'w') as f:
+#    for make in linksofcarmakes:
+#        f.write(make)
+#        f.write('\n')
+#        for model in linksofcarmodels:
+#            if model.split('/')[2] == make:
+#                f.write(model)
+#                f.write('\n')
+   
+
+#    print("data exported")
 
 
 
